b8a9361e68961db6ca1e530b5332e95893f0ff17
Stefan Heiden <stefan.heiden@qudosoft.de>
2021-03-30 17:17:13 +0200

    intial

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/polpettone/pi3
+
+go 1.16
+
+require go.i3wm.org/i3/v4 v4.18.0
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"go.i3wm.org/i3/v4"
+	"log"
+	"sort"
+)
+
+type Overview struct {
+	WorkspaceOverviews []WorkspaceOverview
+}
+
+func (overview Overview) format() string {
+	m := make(map[string][]WorkspaceOverview)
+
+	for _, w := range overview.WorkspaceOverviews {
+		m[w.Screen] = append(m[w.Screen], w)
+	}
+
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	var output string
+	for _, k := range keys {
+		output += fmt.Sprintf("%s \n", k)
+		for _, w := range m[k] {
+			output += fmt.Sprintf("-- %s %s \n", w.Name, w.Programs)
+		}
+	}
+	return output
+}
+
+type WorkspaceOverview struct {
+	Screen   string
+	Name     string
+	Programs []string
+}
+
+func main() {
+	tree, err := i3.GetTree()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	workspaces, err := i3.GetWorkspaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var workspaceOverviews []WorkspaceOverview
+
+	for _, w := range workspaces {
+		found := tree.Root.FindChild(func(n *i3.Node) bool {
+			return int64(n.ID) == int64(w.ID)
+		})
+		wo := WorkspaceOverview{
+			Screen: w.Output,
+			Name:   w.Name,
+		}
+
+		for _, sn := range found.Nodes {
+			wo.Programs = append(wo.Programs, sn.Name)
+		}
+
+		workspaceOverviews = append(workspaceOverviews, wo)
+	}
+
+	overview := Overview{
+		WorkspaceOverviews: workspaceOverviews,
+	}
+
+	fmt.Printf("%s", overview.format())
+
+}
