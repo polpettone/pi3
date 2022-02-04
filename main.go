06960de1d6bad40d340e04fc581564bec6489ada
Stefan Heiden <stefanheiden@gmail.com>
2022-02-04 17:17:31 +0100

    new implementation

--- main.go
+++ main.go
@@ -3,138 +3,65 @@ package main
 import (
 	"fmt"
 	"log"
-	"sort"
 
 	"go.i3wm.org/i3/v4"
 )
 
-type Overview struct {
-	WorkspaceOverviews []WorkspaceOverview
-}
-
-func (overview Overview) printFormatted() {
-	m := make(map[string][]WorkspaceOverview)
-
-	for _, w := range overview.WorkspaceOverviews {
-		m[w.Screen] = append(m[w.Screen], w)
-	}
-
-	var keys []string
-	for k := range m {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	for _, k := range keys {
-		fmt.Printf("%s \n", k)
-		for _, w := range m[k] {
-			fmt.Printf("\n")
-			fmt.Printf("- %s \n", w.Name)
-			for _, p := range w.Programs {
-				if p.Focused {
-					fmt.Printf("  %s \n", "-----------------")
-					fmt.Printf("  %s : %s \n", p.Title, p.Instance)
-					fmt.Printf("  %s \n", "-----------------")
-				} else {
-					fmt.Printf("  %s : %s \n", p.Title, p.Instance)
-				}
-			}
-		}
-		fmt.Printf("\n")
-	}
-}
-
-type WorkspaceOverview struct {
-	Screen   string
-	Name     string
-	Programs []Program
-}
-
-type Program struct {
-	Name     string
-	Instance string
-	Title    string
-	Focused  bool
-}
-
-func collect() {
+func printOverview() {
 
 	tree, err := i3.GetTree()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	workspaces, err := i3.GetWorkspaces()
-	if err != nil {
-		log.Fatal(err)
-	}
+	outputs := []*i3.Node{}
+	outputs = allNodesOfType("output", tree.Root, outputs)
 
-	var workspaceOverviews []WorkspaceOverview
+	for _, output := range outputs {
 
-	for _, w := range workspaces {
-		found := tree.Root.FindChild(func(n *i3.Node) bool {
-			return int64(n.ID) == int64(w.ID)
-		})
+		if output.Name != "__i3" {
 
-		wo := WorkspaceOverview{
-			Screen: w.Output,
-			Name:   w.Name,
-		}
+			fmt.Printf("%s \n", output.Name)
 
-		//TODO: walk down the tree
-		for _, sn := range found.Nodes {
-			if len(sn.Nodes) == 0 {
-				program := Program{
-					sn.Name,
-					sn.WindowProperties.Instance,
-					sn.WindowProperties.Title,
-					sn.Focused,
+			workspaces := []*i3.Node{}
+			workspaces = allNodesOfType("workspace", output, workspaces)
+
+			for _, workspace := range workspaces {
+				fmt.Printf("  - %s \n", workspace.Name)
+
+				contents := []*i3.Node{}
+				contents = allContentNodes(workspace, contents)
+
+				for _, content := range contents {
+					fmt.Printf("    - %s %s \n", content.Name, content.WindowProperties.Instance)
 				}
-				wo.Programs = append(wo.Programs, program)
-			} else {
-
 			}
 		}
-
-		workspaceOverviews = append(workspaceOverviews, wo)
 	}
-
-	overview := Overview{
-		WorkspaceOverviews: workspaceOverviews,
-	}
-
-	overview.printFormatted()
 }
 
-func printI3Tree() {
-	tree, err := i3.GetTree()
-	if err != nil {
-		log.Fatal(err)
-	}
-	printNode(tree.Root)
-}
-
-func printNode(node *i3.Node) {
-	if len(node.Nodes) == 0 {
-
-		if node.Type == "con" && node.Name != "" {
-			fmt.Printf("--- %s \n", node.Name)
-		}
-
+func allContentNodes(node *i3.Node, nodes []*i3.Node) []*i3.Node {
+	if node.Type == "con" && node.Name != "" {
+		nodes = append(nodes, node)
 	} else {
 		for _, n := range node.Nodes {
-			if node.Type == "workspace" {
-				fmt.Println()
-				fmt.Printf("%s:  %s \n", "Workspace", node.Name)
-			}
-			if node.Type == "output" {
-				fmt.Println()
-				fmt.Printf("%s:  %s \n", "Screen", node.Name)
-			}
-			printNode(n)
+			nodes = allContentNodes(n, nodes)
 		}
 	}
+	return nodes
+}
+
+func allNodesOfType(nodeType string, node *i3.Node, nodes []*i3.Node) []*i3.Node {
+	if string(node.Type) != nodeType {
+		for _, n := range node.Nodes {
+			nodes = allNodesOfType(nodeType, n, nodes)
+		}
+	} else {
+		nodes = append(nodes, node)
+	}
+	return nodes
 }
 
 func main() {
-	printI3Tree()
+	printOverview()
 }
